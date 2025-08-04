2518a0e8581941709db826ddc63d4a7e58462ed4
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-08-04 22:04:18 +0530

    Update WeatherDisplay.jsx

--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -1,1 +1,21 @@
+import React from 'react'
 
+function WeatherDisplay(props) {
+    const temp=props.info.temperature
+    const condition=props.info.condition
+    const color=temp>20? "red":"blue"
+  return (
+    <>
+    <p>Temperature: <span style={{color:color}}>{temp}</span></p>
+    <p>Condition:{condition}</p>
+    {/* <div>
+    if({props.temeperature>20})
+    <p><span style={{color:"red"}}>{props.temperature}</span></p>
+    else
+    <p><span style={{color:"blue"}}>{props.temperature}</span></p>
+    </div> */}
+    </>
+  )
+}
+
+export default WeatherDisplay
